eded49444878134a89d515bb2ab05fcf5585a38b
Marvin Fernandez <marvinero@gmail.com>
2018-11-12 19:35:27 -0400

    update test java

--- src/test/java/FechaTest.java
+++ src/test/java/FechaTest.java
@@ -1,13 +1,40 @@
-import org.junit.Assert;
-import org.junit.Test;
+import java.util.Scanner;
 
-public class FechaTest {
-    @Test
-    public void testFecha(){
-        Fechas fechas = new Fechas('28/11/2018');
-        fechas.validarFecha("28/11/2018");
-        String actual = ;
-        String expect = ("28/11/2018");
-        Assert.assertEquals(expect,actual);
+public class Test_fecha {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int d, m, a;
+
+        //Se pide por teclado el dia, mes y año
+        System.out.println("Introduce fecha: ");
+        System.out.print("dia: ");
+        d = sc.nextInt();
+        System.out.print("mes: ");
+        m = sc.nextInt();
+        System.out.print("año: ");
+        a = sc.nextInt();
+
+        //Se crea un objeto Fecha utilizando el consructor con parámetros
+
+        Fecha fecha = new Fecha(d,m,a);
+
+        if (fecha.fechaCorrecta()) { //si la fecha es correcta
+
+            //Se muestra utilizando el método toString()
+
+            System.out.println("Fecha introducida: " + fecha);
+
+            //Se muestran los 2 días siguientes utilizando el método diaSiguiente()
+
+            System.out.println("Los 2 días siguientes son:");
+            for (int i = 1; i <= 2; i++) {
+                fecha.diaSiguiente();
+                System.out.println(fecha);
+            }
+
+        } else { //Si la fecha no es correcta
+            System.out.println("Fecha no valida");
+        }
     }
 }
